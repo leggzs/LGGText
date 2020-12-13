a388f4bf0e1cf2f4043a8e39ef223a0c8028421f
lianle <111@126.com>
2020-12-13 23:44:18 +0800

    bug1.0.0fix

---
+++ LGGText/LGGText/LGGBug.h
@@ -0,0 +1,17 @@
+//
+//  LGGBug.h
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGGBug : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LGGText/LGGText/LGGBug.m
@@ -0,0 +1,13 @@
+//
+//  LGGBug.m
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import "LGGBug.h"
+
+@implementation LGGBug
+
+@end
