e1187040de930bbc6883441cb7f517c1a6fc1e63
zhenglianle <1109432162@qq.com>
2020-12-13 22:43:36 +0800

    创建了LGGHahaView文件

---
+++ LGGText/LGGText/LGGHahaView.h
@@ -0,0 +1,17 @@
+//
+//  LGGHahaView.h
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGGHahaView : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LGGText/LGGText/LGGHahaView.m
@@ -0,0 +1,13 @@
+//
+//  LGGHahaView.m
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import "LGGHahaView.h"
+
+@implementation LGGHahaView
+
+@end
