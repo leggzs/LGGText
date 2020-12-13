dc5c98171bd308d270805d2178f14b5e4a48ae89
lianle <111@126.com>
2020-12-13 22:58:08 +0800

    创建hehe文件

---
+++ LGGText/LGGText/LGGHehe.h
@@ -0,0 +1,17 @@
+//
+//  LGGHehe.h
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGGHehe : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LGGText/LGGText/LGGHehe.m
@@ -0,0 +1,13 @@
+//
+//  LGGHehe.m
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/13.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import "LGGHehe.h"
+
+@implementation LGGHehe
+
+@end
