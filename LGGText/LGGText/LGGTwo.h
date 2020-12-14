8ac7c6578ddf23f5e4ceb7154ce6cca381e2f9c3
lianle <111@126.com>
2020-12-14 23:03:24 +0800

    2.0.0开发

---
+++ LGGText/LGGText/LGGTwo.h
@@ -0,0 +1,17 @@
+//
+//  LGGTwo.h
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/14.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGGTwo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LGGText/LGGText/LGGTwo.m
@@ -0,0 +1,13 @@
+//
+//  LGGTwo.m
+//  LGGText
+//
+//  Created by 郑连乐 on 2020/12/14.
+//  Copyright © 2020 beijingshuoji. All rights reserved.
+//
+
+#import "LGGTwo.h"
+
+@implementation LGGTwo
+
+@end
